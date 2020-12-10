270b2404a3ea8f8edd09d8c790b53deb6b9f256d
HieuJB <trunghieuit061099@gmail.com>
2020-12-10 23:44:21 +0700

    update-navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,28 +1,24 @@
 import React,{} from "react";
-import Logo from './Picture/logo1.png'
 import "./css.css";
 export default function Navbar(){
     return(
         <div className="Navbarr">
             <div className="containerr">
                 <div className="logo-iconss">
-                   <img src={Logo}  alt=""></img>
-                   <p>React-Native</p>
+                   <img src="https://clipartstation.com/wp-content/uploads/2018/09/clipart-toko-1.png"  alt=""></img>
+                   <a href="dsa">JB-STORE</a>
                 </div>
                 <div className="card-icon">
-                   
-                        <a href="as"><i className="fa fa-shopping-cart"></i><p>Cart</p></a>
-                   
+                    <a href="as"><i className="fa fa-shopping-cart"></i><p>Cart</p></a>
                 </div>
                 <div className="create-icon">
-                <a href="as"><i className='fas fa-plus-circle'></i><p>Cart</p></a>
-
+                    <a href="as"><i className='fas fa-plus-circle'></i><p>Cart</p></a>
                 </div>
                 <div className="account-icon">
-                  
+                    <a href="as"><i className='fa fa-user'></i><p>Cart</p></a>
                 </div>
                 <div className="logout-icon">
-                  
+                    <a href="as"><i className='fa fa-sign-out'></i><p>Cart</p></a>
                 </div>
             </div>    
         </div>
