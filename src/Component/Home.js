15558a33f17fc2f61eb3a670a618ea65160f9670
HieuJB <trunghieuit061099@gmail.com>
2020-12-11 21:16:13 +0700

    update

---
+++ src/Component/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./css.css";
+export default function Home(){
+    return(
+        <div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>dsad</th>
+                        <th>dasd</th>
+                        <th>dsadsa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">dasdsa</td>
+                        <td>dsadas</td>
+                        <td>dasdsa</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">dasdsa</td>
+                        <td>dsadas</td>
+                        <td>dsadsa</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,25 +1,27 @@
 import React,{} from "react";
 import "./css.css";
+import Home from "./Home";
 export default function Navbar(){
     return(
         <div className="Navbarr">
             <div className="containerr">
                 <div className="logo-iconss">
                    <img src="https://clipartstation.com/wp-content/uploads/2018/09/clipart-toko-1.png"  alt=""></img>
-                   <a href="dsa">JB-STORE</a>
+                   <a href="ss">JB-STORE</a>
                 </div>
                 <div className="card-icon">
-                    <a href="as"><i className="fa fa-shopping-cart"></i><p>Cart</p></a>
+                    <a href="dasd"><i className="fa fa-shopping-cart"></i><p>Cart</p></a>
                 </div>
                 <div className="create-icon">
-                    <a href="as"><i className='fas fa-plus-circle'></i><p>Cart</p></a>
+                    <a href="as"><i className='fas fa-plus-circle'></i><p>Create</p></a>
                 </div>
                 <div className="account-icon">
-                    <a href="as"><i className='fa fa-user'></i><p>Cart</p></a>
+                    <a href="as"><i className='fa fa-user'></i><p>Account</p></a>
                 </div>
                 <div className="logout-icon">
-                    <a href="as"><i className='fa fa-sign-out'></i><p>Cart</p></a>
+                    <a href="as"><i className='fa fa-sign-out'></i><p>Logout</p></a>
                 </div>
+                <a href="/cart">dsadsa</a>
             </div>    
         </div>
     )
